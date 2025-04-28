1d861242780ff86e6057ff159e075dabf66bfbc0
hime2828 <ramidisaisuhas@gmail.com>
2025-04-28 14:55:24 +0530

    service

---
+++ src/components/ServiceCard.jsx
@@ -0,0 +1,18 @@
+
+import React from "react"
+
+
+function ServiceCard(data) = {
+  const ServiceCard = ({data})=>{
+
+  return (
+    <>
+    <div>
+    <h3>{data.service}</h3>
+    <p>ServiceName: {data.description}</p>
+      </div>
+    </>
+  )
+}}
+
+export default App
